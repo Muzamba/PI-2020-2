ce1f3397e99babb677bbc552aefd0e59baa65ba4
Ariel Batista da Silva <batistariel98@gmail.com>
2020-09-21 09:27:47 -0300

    Correção de bugs/Player2

--- Assets/Player/Flecha.cs
+++ Assets/Player/Flecha.cs
@@ -5,15 +5,18 @@ using UnityEngine;
 public class Flecha : MonoBehaviour
 {
     private Rigidbody2D rigid;
+    public float delayDestroy;
+    
     // Start is called before the first frame update
     void Start()
     {
         rigid = GetComponent<Rigidbody2D>();
+        GameObject.Destroy(gameObject,delayDestroy);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.rotation = Quaternion.Euler(0,0,Vector2.Angle(Vector2.down,rigid.velocity));
+        transform.rotation = Quaternion.Euler(0,0,Vector2.SignedAngle(Vector2.down,rigid.velocity));
     }
 }
--- Assets/Player/Player.cs
+++ Assets/Player/Player.cs
@@ -13,6 +13,9 @@ public class Player : MonoBehaviour
     public int totalPulos;
     public float pulo;
     public float flechaForca;
+    public float veloLimite;
+
+    public int player;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,49 +26,126 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-        if (Input.GetKey(KeyCode.Space) || Input.GetKeyUp(KeyCode.Space))
+
+        switch (player)
         {
+            case 0:
+                if (Input.GetKey(KeyCode.Space) || Input.GetKeyUp(KeyCode.Space))
+                {
             
-            if (Input.GetKeyUp(KeyCode.Space))
-            {
-                var arrow = ArrowDirection();
-                var go = Instantiate(flecha, transform.position + Vector3.right * delta, arrow);
-                go.GetComponent<Rigidbody2D>().AddForce((arrow * Vector2.down) * flechaForca);
-                Physics2D.IgnoreCollision(go.GetComponent<Collider2D>(), GetComponent<Collider2D>());
+                    if (Input.GetKeyUp(KeyCode.Space))
+                    {
+                        var arrow = ArrowDirection();
+                        var go = Instantiate(flecha, transform.position + Vector3.right * delta, arrow);
+                        go.GetComponent<Rigidbody2D>().AddForce((arrow * Vector2.down) * flechaForca);
+                        Physics2D.IgnoreCollision(go.GetComponent<Collider2D>(), GetComponent<Collider2D>());
                 
-            }
+                    }
              
             
-        }
-        else
-        {
+                }
+                else
+                {
+                    //Movimentação
             
-            if (Input.GetKey(KeyCode.A))
-            {
-                rigidbody.position += Vector2.left * (velocidade * Time.deltaTime);
-            }
+                    if (Input.GetKey(KeyCode.A))
+                    {
+                        rigidbody.velocity += Vector2.left * (velocidade * Time.deltaTime);
+                        if (rigidbody.velocity.magnitude > veloLimite)
+                        {
+                            rigidbody.velocity -= Vector2.left * (velocidade * Time.deltaTime);
+                        }
+                    }
 
-            if (Input.GetKeyDown(KeyCode.W) && pulosDisponiveis > 0)
-            {
-                rigidbody.velocity = new Vector2(rigidbody.velocity.x, 0);
-                rigidbody.AddForce(Vector2.up * pulo);
-                pulosDisponiveis--;
+                    if (Input.GetKeyDown(KeyCode.W) && pulosDisponiveis > 0)
+                    {
+                        rigidbody.velocity = new Vector2(rigidbody.velocity.x, 0);
+                        rigidbody.AddForce(Vector2.up * pulo);
+                        pulosDisponiveis--;
 
-            }
+                    }
 
-            if (Input.GetKey(KeyCode.D))
-            {
-                rigidbody.position += Vector2.right * (velocidade * Time.deltaTime);
-            }
+                    if (Input.GetKey(KeyCode.D))
+                    {
+                        rigidbody.velocity += Vector2.right * (velocidade * Time.deltaTime);
+                        if (rigidbody.velocity.magnitude > veloLimite)
+                        {
+                            rigidbody.velocity -= Vector2.right * (velocidade * Time.deltaTime);
+                        }
+                    }
 
-            if (Input.GetKey(KeyCode.S))
-            {
-                //rigidbody.position += Vector2.down * (velocidade * Time.deltaTime);
-            }
+                    if (Input.GetKey(KeyCode.S))
+                    {
+                        //rigidbody.position += Vector2.down * (velocidade * Time.deltaTime);
+                    }
+            
+            
+                }//else
+                break;
+            
+            case 1:
+                if (Input.GetKey(KeyCode.RightControl) || Input.GetKeyUp(KeyCode.RightControl))
+                {
+            
+                    if (Input.GetKeyUp(KeyCode.RightControl))
+                    {
+                        var arrow = ArrowDirection();
+                        var go = Instantiate(flecha, transform.position + Vector3.left * delta, arrow);
+                        go.GetComponent<Rigidbody2D>().AddForce((arrow * Vector2.down) * flechaForca);
+                        Physics2D.IgnoreCollision(go.GetComponent<Collider2D>(), GetComponent<Collider2D>());
+                
+                    }
+             
+            
+                }
+                else
+                {
+                    //Movimentação
+            
+                    if (Input.GetKey(KeyCode.LeftArrow))
+                    {
+                        rigidbody.velocity += Vector2.left * (velocidade * Time.deltaTime);
+                        if (rigidbody.velocity.magnitude > veloLimite)
+                        {
+                            rigidbody.velocity -= Vector2.left * (velocidade * Time.deltaTime);
+                        }
+                    }
+
+                    if (Input.GetKeyDown(KeyCode.UpArrow) && pulosDisponiveis > 0)
+                    {
+                        rigidbody.velocity = new Vector2(rigidbody.velocity.x, 0);
+                        rigidbody.AddForce(Vector2.up * pulo);
+                        pulosDisponiveis--;
+
+                    }
+
+                    if (Input.GetKey(KeyCode.RightArrow))
+                    {
+                        rigidbody.velocity += Vector2.right * (velocidade * Time.deltaTime);
+                        if (rigidbody.velocity.magnitude > veloLimite)
+                        {
+                            rigidbody.velocity -= Vector2.right * (velocidade * Time.deltaTime);
+                        }
+                    }
+
+                    if (Input.GetKey(KeyCode.DownArrow))
+                    {
+                        //rigidbody.position += Vector2.down * (velocidade * Time.deltaTime);
+                    }
+            
+            
+                }//else
+                break;
+            
+            default:
+                break;
+            
             
         }
-    }
+        
+        
+        
+    }//Update
 
     private void OnCollisionEnter2D(Collision2D other)
     {
@@ -78,44 +158,100 @@ public class Player : MonoBehaviour
 
     private Quaternion ArrowDirection()
     {
-        Quaternion resultado = Quaternion.Euler(0,0,90);
-        if (Input.GetKey(KeyCode.W))
+        Quaternion resultado = Quaternion.identity;
+        switch (player)
         {
-            if (Input.GetKey(KeyCode.D))
-            {
-                resultado = Quaternion.Euler(0,0,135);
-            } else if (Input.GetKey(KeyCode.A))
-            {
-                resultado = Quaternion.Euler(0, 0, 225);
-            }
-            else
-            {
-                resultado = Quaternion.Euler(0, 0, 180);
-            }
-        }else if (Input.GetKey(KeyCode.S))
-        {
-            if (Input.GetKey(KeyCode.D))
-            {
-                resultado = Quaternion.Euler(0,0,45);
-            } else if (Input.GetKey(KeyCode.A))
-            {
-                resultado = Quaternion.Euler(0, 0, 315);
-            }
-            else
-            {
-                resultado = Quaternion.Euler(0, 0, 0);
-            }
+            case 0:
+                Quaternion resultado0 = Quaternion.Euler(0,0,90);
+                if (Input.GetKey(KeyCode.W))
+                {
+                    if (Input.GetKey(KeyCode.D))
+                    {
+                        resultado0 = Quaternion.Euler(0,0,135);
+                    } else if (Input.GetKey(KeyCode.A))
+                    {
+                        resultado0 = Quaternion.Euler(0, 0, 225);
+                    }
+                    else
+                    {
+                        resultado0 = Quaternion.Euler(0, 0, 180);
+                    }
+                }else if (Input.GetKey(KeyCode.S))
+                {
+                    if (Input.GetKey(KeyCode.D))
+                    {
+                        resultado0 = Quaternion.Euler(0,0,45);
+                    } else if (Input.GetKey(KeyCode.A))
+                    {
+                        resultado0 = Quaternion.Euler(0, 0, 315);
+                    }
+                    else
+                    {
+                        resultado0 = Quaternion.Euler(0, 0, 0);
+                    }
+                }
+                else
+                {
+                    if (Input.GetKey(KeyCode.D))
+                    {
+                        resultado0 = Quaternion.Euler(0,0,90);
+                    } else if (Input.GetKey(KeyCode.A))
+                    {
+                        resultado0 = Quaternion.Euler(0, 0, 270);
+                    }
+                }
+
+                resultado = resultado0;
+                break;
+            
+            case 1:
+                Quaternion resultado1 = Quaternion.Euler(0,0,270);
+                if (Input.GetKey(KeyCode.UpArrow))
+                {
+                    if (Input.GetKey(KeyCode.RightArrow))
+                    {
+                        resultado1 = Quaternion.Euler(0,0,135);
+                    } else if (Input.GetKey(KeyCode.LeftArrow))
+                    {
+                        resultado1 = Quaternion.Euler(0, 0, 225);
+                    }
+                    else
+                    {
+                        resultado1 = Quaternion.Euler(0, 0, 180);
+                    }
+                }else if (Input.GetKey(KeyCode.DownArrow))
+                {
+                    if (Input.GetKey(KeyCode.RightArrow))
+                    {
+                        resultado1 = Quaternion.Euler(0,0,45);
+                    } else if (Input.GetKey(KeyCode.LeftArrow))
+                    {
+                        resultado1 = Quaternion.Euler(0, 0, 315);
+                    }
+                    else
+                    {
+                        resultado1 = Quaternion.Euler(0, 0, 0);
+                    }
+                }
+                else
+                {
+                    if (Input.GetKey(KeyCode.RightArrow))
+                    {
+                        resultado1 = Quaternion.Euler(0,0,90);
+                    } else if (Input.GetKey(KeyCode.LeftArrow))
+                    {
+                        resultado1 = Quaternion.Euler(0, 0, 270);
+                    }
+                }
+
+                resultado = resultado1;
+                break;
+            
+            default:
+                break;
+            
         }
-        else
-        {
-            if (Input.GetKey(KeyCode.D))
-            {
-                resultado = Quaternion.Euler(0,0,90);
-            } else if (Input.GetKey(KeyCode.A))
-            {
-                resultado = Quaternion.Euler(0, 0, 270);
-            }
-        }
+        
 
         return resultado;
     }
