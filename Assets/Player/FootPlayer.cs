32d814502869f98fc9afa8570cfa2b9d441a6dd2
Ariel Batista da Silva <batistariel98@gmail.com>
2020-10-24 06:58:33 -0300

    movement refactor
    Close: #9

---
+++ Assets/Player/FootPlayer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Chao"))
+        {
+            GetComponentInParent<NewPlayer>().Grounded = true;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Chao"))
+        {
+            GetComponentInParent<NewPlayer>().Grounded = false;
+        }
+    }
+}
