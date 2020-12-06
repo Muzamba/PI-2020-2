1d53dd3c0fb52a3c33190fd06666082c19065a9b
Ariel Batista da Silva <batistariel98@gmail.com>
2020-12-06 16:19:22 -0300

    outlines coloridas feitas
    #Close: 27

--- Assets/Player/Scripts/NewPlayer.cs
+++ Assets/Player/Scripts/NewPlayer.cs
@@ -111,6 +111,7 @@ public class NewPlayer : MonoBehaviour
         }
 
         Grounded = false;
+        
 
     }
 
---
+++ Assets/Player/Scripts/SpritePlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpritePlayer : MonoBehaviour
+{
+    [SerializeField] private Material _material;
+    void Start()
+    {
+        GetComponent<SpriteRenderer>().material = Instantiate(_material);
+        GetComponent<SpriteRenderer>().material.color = GetComponentsInParent<Arqueira>()[0].cor;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
